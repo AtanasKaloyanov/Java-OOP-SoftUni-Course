46673880df30ab4a6252930aed50ab3f0e00206b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-06-28 18:26:38 +0300

    This is the solution of the problem "Rhombus of Stars".

---
+++ Lab/P01RhombusOfStars.java
@@ -0,0 +1,31 @@
+package T01WorkingWithAbstraction.Lab;
+
+import java.util.Scanner;
+
+public class P01RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int givenNumber = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= givenNumber ; i++) {
+            for (int j = 1; j <= givenNumber - i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = givenNumber - 1; i > 0 ; i--) {
+            for (int j = 1; j <= givenNumber - i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
