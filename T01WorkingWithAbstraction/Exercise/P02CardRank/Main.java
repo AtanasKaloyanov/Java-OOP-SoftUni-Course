e0200b0e921c118ca03dc684e32f77e6a919a259
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-07-09 15:45:57 +0300

    This is the solution of the problem "Card Rank".

---
+++ T01WorkingWithAbstraction/Exercise/P02CardRank/Main.java
@@ -0,0 +1,12 @@
+package T01WorkingWithAbstraction.Exercise.P02CardRank;
+
+public class Main {
+    public static void main(String[] args) {
+         
+        CardRank[] cardRank = CardRank.values();
+        System.out.println("Card Ranks:");
+        for (CardRank rank : cardRank) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", rank.ordinal(), rank);
+        }
+    }
+}
