61faac67eeecd44988536dd2f5c654fef9c7c8fb
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-07-21 10:40:00 +0300

    This is the solution of the problem "Players and Monsters".

---
+++ T03Inheritance/Exercise/P03PlayersAndMonsters/Main.java
@@ -0,0 +1,8 @@
+package T03Inheritance.Exercise.P03PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+        MuseElf museElf = new MuseElf("Denica", 69);
+        System.out.println(museElf);
+    }
+}
