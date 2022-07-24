6c83c841df95ae70fd58b386c4d112fc63b5f842
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-07-24 18:46:57 +0300

    This is the solution of the problem "Math Operation".

---
+++ T05Polymorphism/Lab/P01MathOperation/MathOperation.java
@@ -0,0 +1,15 @@
+package T05Polymorphism.Lab.P01MathOperation;
+
+public class MathOperation {
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int add(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+
+    public int add(int number1, int number2, int number3, int number4) {
+        return number1 + number2 + number3 + number4;
+    }
+}
