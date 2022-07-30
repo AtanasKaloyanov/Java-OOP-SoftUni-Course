24a43ccda475c78bff70450df11ab9c5caef4fd5
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-07-30 14:30:02 +0300

    This is the solution of the problem "Number in Range".

---
+++ T08ExceptionAndErrorHandling/P01/P01NumberInRange.java
@@ -0,0 +1,36 @@
+package T08ExceptionAndErrorHandling.P01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01NumberInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] range = Arrays.stream(scanner.nextLine().split("\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int start = range[0];
+        int end = range[1];
+
+        System.out.printf("Range: [%d...%d]%n", start, end);
+
+        String input = scanner.nextLine();
+        while (true) {
+            try {
+                int number = Integer.parseInt(input);
+                if (number >= start && number <= end) {
+                    System.out.printf("Valid number: %d%n", number);
+                    return;
+                } else {
+                    System.out.printf("Invalid number: %s%n", input);
+                }
+
+            } catch (Exception exception) {
+                System.out.printf("Invalid number: %s%n", input);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
