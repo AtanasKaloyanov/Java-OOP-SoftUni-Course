eb971b2d753c092e7c8fe966fca2ef481edd719b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-08-01 21:03:25 +0300

    refactoring test for the dummy and the axe

--- T10UnitTesting/test/java/PRpg/AxeTest.java
+++ T10UnitTesting/test/java/PRpg/AxeTest.java
@@ -1,24 +1,43 @@
 package PRpg;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+
 public class AxeTest {
+    private final static int AXE_ATTACK_POINTS = 10;
+    private final static int AXE_DURABILITY_POINTS = 10;
+    private final static int BROKEN_AXE_DURABILITY_POINTS = 0;
+    private final static int DECREASING_DURABILITY_BY_EVERY_ATTACK = 1;
+
+    private final static int DUMMY_HEALTH = 10;
+    private final static int DUMMY_EXPERIENCE = 10;
+
+
+    private Axe axe;
+    private Axe brokenAxe;
+    private Dummy dummy;
+
+    @Before
+    public void setup() {
+        axe = new Axe(AXE_ATTACK_POINTS, AXE_DURABILITY_POINTS);
+        brokenAxe = new Axe(AXE_ATTACK_POINTS, BROKEN_AXE_DURABILITY_POINTS);
+        dummy = new Dummy(DUMMY_HEALTH, DUMMY_EXPERIENCE);
+    }
+
     @Test
     public void testAxeLosesDurabilityAfterAttack() {
-        Axe axe = new Axe(10, 10);
-        Dummy dummy = new Dummy(10, 10);
-
+        int durabilityBeforeAttack = axe.getDurabilityPoints();
         axe.attack(dummy);
-        Assert.assertEquals(9, axe.getDurabilityPoints());
+        Assert.assertEquals(durabilityBeforeAttack - DECREASING_DURABILITY_BY_EVERY_ATTACK, axe.getDurabilityPoints());
     }
 
     @Test(expected = IllegalStateException.class)
     public void testAttackWithBrokenAxeShouldThrowException() {
-        Axe axe = new Axe(10, 0);
         Dummy dummy = new Dummy(10, 10);
 
-        axe.attack(dummy);
+        brokenAxe.attack(dummy);
     }
 
 }
