50fbe7e2147f15dcada3b14245acd6edbb273b26
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-13 22:41:41 +0300

    This is the updated solution of the problem "Card Rank".

--- T01WorkingWithAbstraction/Exercise/P02CardRank/Main.java
+++ T01WorkingWithAbstraction/Exercise/P02CardRank/Main.java
@@ -1,12 +1,20 @@
 package T01WorkingWithAbstraction.Exercise.P02CardRank;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-         
-        CardRank[] cardRank = CardRank.values();
-        System.out.println("Card Ranks:");
-        for (CardRank rank : cardRank) {
-            System.out.printf("Ordinal value: %d; Name value: %s%n", rank.ordinal(), rank);
+        // 1. Command reading:
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        // 2. Iterating over the enum values and printing the
+        // current index and the value itself:
+        System.out.println(command + ":");
+        for (Rank value : Rank.values()) {
+            int index = value.ordinal();
+            String name = value.name();
+            System.out.printf("Ordinal value: %d; Name value: %s\n", index, name);
         }
     }
 }
---
+++ T01WorkingWithAbstraction/Exercise/P02CardRank/Rank.java
@@ -0,0 +1,8 @@
+package T01WorkingWithAbstraction.Exercise.P02CardRank;
+
+public enum Rank {
+    ACE, TWO, THREE, FOUR,
+    FIVE, SIX, SEVEN, EIGHT,
+    NINE, TEN, JACK, QUEEN,
+    KING
+}
