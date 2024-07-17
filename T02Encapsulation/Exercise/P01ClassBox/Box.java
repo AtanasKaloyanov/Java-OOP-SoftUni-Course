f37a01f67d3327cb4a21a82187d747e22611007c
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-17 12:56:28 +0300

    This is the updated solution of the problem "Class Box".

--- T02Encapsulation/Exercise/P01ClassBox/Box.java
+++ T02Encapsulation/Exercise/P01ClassBox/Box.java
@@ -13,41 +13,47 @@ public class Box {
 
     private void setLength(double length) {
         if (length <= 0) {
-            throw new IllegalArgumentException("Length cannot be zero or negative.");
+            throw new IllegalArgumentException("Length cannot be zero or negative");
         }
         this.length = length;
     }
 
     private void setWidth(double width) {
         if (width <= 0) {
-            throw new IllegalArgumentException("Width cannot be zero or negative.");
+            throw new IllegalArgumentException("Width cannot be zero or negative");
         }
         this.width = width;
     }
 
     private void setHeight(double height) {
         if (height <= 0) {
-            throw new IllegalArgumentException("Height cannot be zero or negative.");
+            throw new IllegalArgumentException("Height cannot be zero or negative");
         }
         this.height = height;
     }
 
     public double calculateSurfaceArea() {
-        // Surface Area = 2lw + 2lh + 2wh
-        return 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+        return 2 * this.length * this.width+
+                2 * this.length * this.height +
+                2 * this.width * this.height;
     }
 
     public double calculateLateralSurfaceArea() {
-        //Lateral Surface Area = 2lh + 2wh
-        return 2 * this.length * this.height + 2 * this.width * this.height;
+        return 2 * this.length * this.height +
+                2 * this.width * this.height;
     }
 
     public double calculateVolume() {
-        //Volume = lwh
         return this.length * this.width * this.height;
     }
-
-    //calculateSurfaceArea (): double
-    //calculateLateralSurfaceArea (): double
-    //calculateVolume (): double
 }
+
+/*
++ 	Box (double length, double width, double height)
+-	setLength(double): void
+-	setWidth(double): void
+-	setHeight(double): void
++	calculateSurfaceArea (): double
++	calculateLateralSurfaceArea (): double
++	calculateVolume (): double
+ */
--- T02Encapsulation/Exercise/P01ClassBox/Main.java
+++ T02Encapsulation/Exercise/P01ClassBox/Main.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Input reading - double length, double width and double height:
         Scanner scanner = new Scanner(System.in);
-
         double length = Double.parseDouble(scanner.nextLine());
         double width = Double.parseDouble(scanner.nextLine());
         double height = Double.parseDouble(scanner.nextLine());
+        // 2. Instantiation of class Box and getting the
 
         Box box = new Box(length, width, height);
+        double surface = box.calculateSurfaceArea();
+        double lateralSurface = box.calculateLateralSurfaceArea();
+        double volume = box.calculateVolume();
 
-        System.out.printf("Surface Area - %.2f%n", box.calculateSurfaceArea());
-        System.out.printf("Lateral Surface Area - %.2f%n", box.calculateLateralSurfaceArea());
-        System.out.printf("Volume - %.2f%n", box.calculateVolume());
+        // 3. Output printing:
+        System.out.printf("Surface Area - %.2f\n", surface);
+        System.out.printf("Lateral Surface Area - %.2f\n", lateralSurface);
+        System.out.printf("Volume - %.2f", volume);
     }
 }
