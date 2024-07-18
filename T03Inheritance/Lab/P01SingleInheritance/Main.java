93456ea8853ee62d8984c3f60e9a1b4b2be27b4d
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-18 23:48:03 +0300

    I've added a comment to the problem "Single Inheritance".

--- T03Inheritance/Lab/P01SingleInheritance/Main.java
+++ T03Inheritance/Lab/P01SingleInheritance/Main.java
@@ -2,8 +2,10 @@ package T03Inheritance.Lab.P01SingleInheritance;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Creating an object of the class Dog and
+        // invoking the methods bark() and eat(). The eat() method is inherited.
         Dog dog = new Dog();
-        dog.eat();
         dog.bark();
+        dog.eat();
     }
 }
