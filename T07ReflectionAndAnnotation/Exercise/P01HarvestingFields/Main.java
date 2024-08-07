90574661c552ac97d8ddc39331790c9e625a0a01
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-08-07 16:54:08 +0300

    This is the updated solution of the problem "Harvesting Fields".

--- T07ReflectionAndAnnotation/Exercise/P01HarvestingFields/Main.java
+++ T07ReflectionAndAnnotation/Exercise/P01HarvestingFields/Main.java
@@ -2,62 +2,84 @@ package T07ReflectionAndAnnotation.Exercise.P01HarvestingFields;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
-    private static final String END_COMMAND = "HARVEST";
-    private static final String PRIVATE_FIELD = "private";
-    private static final String PUBLIC_FIELD = "public";
-    private static final String PROTECTED_FIELD = "protected";
+    private static List<Field> privateFields = new ArrayList<>();
+    private static List<Field> protectedFields = new ArrayList<>();
+    private static List<Field> publicFields = new ArrayList<>();
 
+    private static List<Field> allFields = new ArrayList<>();
 
     public static void main(String[] args) {
-        Class clazz = RichSoilLand.class;
-        Field[] allFileds = clazz.getDeclaredFields();
+        // 1. Obtain the class and all its methods, then all its private, protected and public methods:
+        Class<RichSoilLand> richSoilLandClass = RichSoilLand.class;
+        Field[] fields = richSoilLandClass.getDeclaredFields();
+        allFields = Arrays.stream(fields)
+                .collect(Collectors.toList());
 
+        filterMethodsByModifiers(allFields);
+
+        // 2. Printing methods via while cycle algorithm:
         Scanner scanner = new Scanner(System.in);
-        String input;
-
-        while (!END_COMMAND.equals(input = scanner.nextLine())) {
-            switch (input) {
-                case PRIVATE_FIELD:
-                  print(allFileds, PRIVATE_FIELD);
+        String line = scanner.nextLine();
+        while (!line.equals("HARVEST")) {
+            switch (line) {
+                case "private":
+                    printMethods(privateFields);
                     break;
-
                 case "protected":
-                 print(allFileds, PROTECTED_FIELD);
+                    printMethods(protectedFields);
                     break;
-
                 case "public":
-                    print(allFileds, PUBLIC_FIELD);
+                    printMethods(publicFields);
                     break;
-
                 case "all":
-                    Arrays.stream(allFileds)
-                            .forEach(field -> {
-                                if (Modifier.isPublic(field.getModifiers())) {
-                                    System.out.printf("public %s %s%n", field.getType().getSimpleName(), field.getName());
-                                } else if (Modifier.isPrivate(field.getModifiers())) {
-                                    System.out.printf("private %s %s%n", field.getType().getSimpleName(), field.getName());
-                                } else if (Modifier.isProtected(field.getModifiers())) {
-                                    System.out.printf("protected %s %s%n", field.getType().getSimpleName(), field.getName());
-                                }
-                            });
-
+                    printMethods(allFields);
                     break;
             }
 
-            input = scanner.nextLine();
+            line = scanner.nextLine();
         }
-
-        scanner.close();
     }
 
-    public static void print(Field[] fields, String modifier) {
-        Arrays.stream(fields)
-                .filter(field -> Modifier.toString(field.getModifiers()).equals(modifier))
-                .forEach(field -> System.out.printf("%s %s %s%n", Modifier.toString(field.getModifiers()), field.getType().getSimpleName(), field.getName()));
+    private static void filterMethodsByModifiers(List<Field> allFields) {
+        for (Field field : allFields) {
+            if (Modifier.isPrivate(field.getModifiers())) {
+                privateFields.add(field);
+            } else if (Modifier.isProtected(field.getModifiers())) {
+                protectedFields.add(field);
+            } else if (Modifier.isPublic(field.getModifiers())) {
+                publicFields.add(field);
+            }
+        }
     }
 
+    private static void printMethods(List<Field> fields) {
+        for (Field field : fields) {
+            int modifier = field.getModifiers();
+            String modifierSimpleName = getSimpleName(modifier);
+            String type = field.getType().getSimpleName();
+            String name = field.getName();
+            System.out.printf("%s %s %s\n", modifierSimpleName, type, name);
+        }
+    }
+
+    private static String getSimpleName(int modifier) {
+        return switch (modifier) {
+            case 1 -> "public";
+            case 2 -> "private";
+            case 4 -> "protected";
+            default -> "default";
+        };
+    }
 }
+
+// 1 - public
+// 2 - private
+// 0 - default
+// 4 - protected
